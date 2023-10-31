cd9e11eaeaf9e113951293cbaa774aab0c7b5355
Davi Souza Santos <davii.clink@gmail.com>
2023-10-31 00:05:38 -0300

    finalizado

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,26 +2,23 @@ function calcularEstatistica(){
 
 const tarefas = JSON.parse(localStorage.getItem("tarefa")) || []
 
+    
 const pendentes = tarefas.filter(t => t.concluida == false)
+const qtdePendente = pendentes.length
+document.querySelector("#qtde-pendente").innerHTML = qtdePendente
 
-const qtdePendete = pendentes.length
-
-document.querySelector("#qtde-pendente").innerHTML = qtdePendete
-
-const concluidas = tarefas.filter(t => t.concluida == false)
+    
+const concluidas = tarefas.filter(t => t.concluida == true)
 const qtdeconcluida = concluidas.length
 document.querySelector("#qtde-concluida").innerHTML = qtdeconcluida
 
-
-const totalPontos = tarefas.reduce((total, t) => total += +t.pontos, 0)
+    
+const totalPontos = tarefas.reduce((total, t) => total += +t.nota, 0)
 document.querySelector("#total-pontos").innerHTML = totalPontos 
 
-const meusPontos = concluidas.reduce((total, t) => total += +t.pontos, 0)
+const meusPontos = concluidas.reduce((total, t) => total += +t.nota, 0)
 document.querySelector("#meus-pontos").innerHTML = meusPontos 
 
-console.log(totalPontos)
-console.log(meusPontos)
 }
 
 calcularEstatistica()
-
